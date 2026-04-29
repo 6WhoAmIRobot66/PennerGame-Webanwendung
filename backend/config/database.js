8f0812ee49f9d99698a38a2ab1aeda33df679006
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2026-04-29 23:07:39 +0200

    fix_database&env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,20 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const { DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
-
-// Datenbankverbindung konfigurieren:
-const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, // Prüfe: Heißt es in der .env DB_USER oder DB_USERNAME?
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false 
+      }
+    }
+  }
+);
 
 module.exports = sequelize;
